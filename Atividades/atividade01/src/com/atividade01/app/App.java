3affd6208834b17aa0ca92149ef29ba79af95467
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-08-06 16:59:36 -0300

    Aula do dia 05/08/2025.

---
+++ Atividades/atividade01/src/com/atividade01/app/App.java
@@ -0,0 +1,14 @@
+package com.atividade01.app;
+public class App {
+    public static void main(String[] args) throws Exception {
+        // TODO
+        /*
+         * Crie um programa que receba o nome, peso e altura do usuário,
+         * e calcule o valor do seu IMC (Índice de Massa Corporal),
+         * cuja fórmula é peso/altura*altura, e informe o seu diagnóstico
+         * baseado na tabela do IMC (pesquise na Internet pela tabela).
+         * O programa poderá calcular o IMC de vários usuários,
+         * e deverá ter a opção de sair do programa.
+         */
+    }
+}
