b1245e8a3006da4677b66685151236bafbb21c6a
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-08-11 17:41:28 -0300

    Aula do dia 11/08/2025.

---
+++ Atividades/atividade03/src/com/atividade/app/App.java
@@ -0,0 +1,19 @@
+package com.atividade.app;
+public class App {
+    public static void main(String[] args) throws Exception {
+        // TODO
+        /*
+         * Crie um programa com uma classe chamada Conta, com os seguintes atributos:
+         * - Titular da conta
+         * - CPF do titular
+         * - Agência
+         * - Número da conta
+         * - Saldo
+         * O usuário deverá informar o nome do titular e o cpf, e o programa exibe um menu com as seguintes operações:
+         * - Exibir dados da conta
+         * - Fazer saque
+         * - Fazer depósito
+         * - Sair do programa
+         */
+    }
+}
