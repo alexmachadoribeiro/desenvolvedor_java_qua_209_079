9ec41329b330c4b5ba277d5768220700395f1c7d
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-08-18 17:41:58 -0300

    Aula do dia 18/08/2025.

---
+++ Atividades/atividade05/src/com/atividade05/app/App.java
@@ -0,0 +1,12 @@
+package com.atividade05.app;
+public class App {
+    public static void main(String[] args) throws Exception {
+        // TODO: atividade 05
+        /*
+         * Crie um app para verificar qual o melhor combustível para
+         * um carro flex abastecer.
+         * NOTE: compensa mais abastecer com etanol caso o valor dele seja
+         * até 70% do valor da gasolina.
+         */
+    }
+}
