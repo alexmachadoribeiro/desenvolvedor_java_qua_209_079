cdfe86b7c3f26193d39e5af73ce7c14f2038b8bd
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-09-15 17:41:29 -0300

    Aula do dia 15/09/2025.

--- spring/CRUD-System/src/main/java/com/crud/app/repository/AppRepository.java
+++ spring/CRUD-System/src/main/java/com/crud/app/repository/AppRepository.java
@@ -3,6 +3,7 @@ package com.crud.app.repository;
 import org.springframework.data.repository.CrudRepository;
 import com.crud.app.models.Pessoa;
 
-public interface AppRepository extends CrudRepository<Pessoa, String> {
 
+public interface AppRepository extends CrudRepository<Pessoa, String> {
+    Pessoa findByIdPessoa(long idPessoa);
 }
